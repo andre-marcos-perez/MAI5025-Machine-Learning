ae215a4a72d51aa2af0fa92c4a52039fb3694aaf
Andre Perez <andre.marcos.perez@gmail.com>
2019-06-05 18:30:47 -0300

    refactored perceptron to use gridsearch

--- exercise2/perceptron.py
+++ exercise2/perceptron.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
-import seaborn as sns
 from sklearn.linear_model import Perceptron
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import multilabel_confusion_matrix
 
 # ------------------------------------------------------------------------------
@@ -16,14 +16,19 @@ DATASET = "iris"
 EXTENSION = ".csv"
 DIR = "../dataset/"
 MODEL = "perceptron"
-
 DATAPATH = DIR + DATASET + EXTENSION
 
-# -- Train & Model Hiperparams
+# -- Train
 
-TOL = 1e-1
 SEED = 123
-KFOLDS = 10
+FOLDS = 10
+SPLIT = 0.3
+
+# -- Hiperparams
+
+TOL = [1e-1, 1e-2, 1e-3, 1e-4]
+CLASS_WEIGHT = [None, "balanced"]
+MAX_ITER = [100, 1000, 10000]
 
 # ------------------------------------------------------------------------------
 # -- Main ----------------------------------------------------------------------
@@ -39,52 +44,70 @@ with open(DIR + DATASET + "_names" + EXTENSION, mode="r") as file:
 
 data = pd.read_csv(DATAPATH, names=HEADER)
 features = data.drop(HEADER[-1], axis=1)
-target = data.loc[:, HEADER[-1]]
+target = data.select_dtypes(include=[object])
 data = None
 
 cols = list(features.columns)
 for col in cols:
     features[col] = (features[col] - features[col].mean()) / features[col].std()
 
-try:
-    plot = sns.pairplot(pd.concat([features, target], axis=1, sort=False), hue=HEADER[-1])
-    plot.savefig(DATASET + ".png")
-except Exception as e:
-    print("-- Unable to plot dataset: " + e)
-
 # ----------------------------------------
 # -- Train -------------------------------
 # ----------------------------------------
 
-skf = StratifiedKFold(n_splits=KFOLDS)
-skf.get_n_splits(features, target)
+# Split dataset
 
-model = Perceptron(tol=TOL, random_state=SEED)
+X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=SPLIT, random_state=SEED)
+
+# Train model
+
+slnn = Perceptron(random_state=SEED)
+hyper = {
+    "tol":TOL,
+    "class_weight":CLASS_WEIGHT,
+    "max_iter":MAX_ITER
+}
+#score = {'AUC': make_scorer(roc_auc_score), 'Accuracy': make_scorer(accuracy_score)}
+
+model = GridSearchCV(slnn, param_grid=hyper, n_jobs=-1, iid=True, cv=FOLDS)
+model.fit(X_train, y_train.values.ravel())
+prediction = model.predict(X_test)
+
+# Performance metrics
+
 metrics = dict.fromkeys(["acc", "pre", "sen", "esp", "f1"], 0)
-
-for train_index, test_index in skf.split(features, target):
-    model.fit(features.iloc[train_index, :], target.iloc[train_index])
-    prediction = model.predict(features.iloc[test_index, :])
-    mcm = multilabel_confusion_matrix(target.iloc[test_index], prediction)
-    tn = mcm[:, 0, 0]
-    tp = mcm[:, 1, 1]
-    fn = mcm[:, 1, 0]
-    fp = mcm[:, 0, 1]
-    metrics["acc"] = metrics["acc"] + (tp + tn) / (tp + tn + fp + fn)
-    metrics["pre"] = metrics["pre"] + tp / (tp + fp)
-    metrics["sen"] = metrics["sen"] + tp / (tp + fn)
-    metrics["esp"] = metrics["esp"] + tn / (tn + fp)
-    metrics["f1"] = metrics["f1"] + 2*tp / (2*tp + fp + fn)
-
+mcm = multilabel_confusion_matrix(y_test, prediction)
+tn = mcm[:, 0, 0]
+tp = mcm[:, 1, 1]
+fn = mcm[:, 1, 0]
+fp = mcm[:, 0, 1]
+metrics["acc"] = (tp + tn) / (tp + tn + fp + fn)
+metrics["pre"] = tp / (tp + fp)
+metrics["sen"] = tp / (tp + fn)
+metrics["esp"] = tn / (tn + fp)
+metrics["f1"] = 2*tp / (2*tp + fp + fn)
 for key in metrics:
-    metrics[key] = list(metrics[key] / KFOLDS)
-    metrics[key].append(np.mean(metrics[key]))
+    metrics[key] = np.append(metrics[key], np.mean(metrics[key]))
+metrics["acc"] = np.append(metrics["acc"], (np.sum(tp) + np.sum(tn)) / (np.sum(tp) + np.sum(tn) + np.sum(fp) + np.sum(fn)))
+metrics["pre"] = np.append(metrics["pre"], np.sum(tp) / (np.sum(tp) + np.sum(fp)))
+metrics["sen"] = np.append(metrics["sen"], np.sum(tp) / (np.sum(tp) + np.sum(fn)))
+metrics["esp"] = np.append(metrics["esp"], np.sum(tn) / (np.sum(tn) + np.sum(fp)))
+metrics["f1"] = np.append(metrics["f1"], 2*np.sum(tp) / (2*np.sum(tp) + np.sum(fp) + np.sum(fn)))
 
 # ----------------------------------------
 # -- Report ------------------------------
 # ----------------------------------------
 
-cols = list(target.unique())
-cols.append("Average")
+cols = list(target[HEADER[-1]].unique())
+cols.append("Macro Avg")
+cols.append("Micro Avg")
 report = pd.DataFrame.from_dict(metrics, orient="index", columns=cols)
+
+print("")
+print(" # Best Params")
+print("")
+print(" -- " + str(model.best_params_))
+print("")
+print(" # Performance")
+print("")
 print(report)
